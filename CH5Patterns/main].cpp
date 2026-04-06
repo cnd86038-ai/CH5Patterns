9154a46e712fb433d04f54c28290c0978a92d4de
cnd86038-ai <cnd86038@email.vccs.edu>
2026-04-06 16:47:51 -0400

    Setup with number of rows for pattern A, with left side and right side of the pattern.

---
+++ CH5Patterns/main].cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	const int totalRows = 10;
+
+	//Print Patter A
+	cout << "Pattern A" << endl;
+	for (int row = 1; row <= totalRows; row++)
+	{
+		//Left side of the pattern
+		for (int i = 1; i <= row; i++)
+		{
+			cout << "+";
+		}
+	}
+	//Space between left and right side of the pattern
+	cout << " ";
+
+	//Right side of the pattern
+
+	return 0;
+}
