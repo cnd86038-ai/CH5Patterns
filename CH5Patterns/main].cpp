936370fa64d948726deec47b5d4987f6e30da7cb
cnd86038-ai <cnd86038@email.vccs.edu>
2026-04-06 17:18:56 -0400

    Adjusting Pattern A to just have the first Triangle.

--- CH5Patterns/main].cpp
+++ CH5Patterns/main].cpp
@@ -5,23 +5,17 @@ int main()
 {
 	const int totalRows = 10;
 
-	// Pattern A
+	// Pattern A: right angle at bottom-left
 	cout << "Pattern A" << endl;
-	for (int row = 1; row <= totalRows; row++)
-	{
-		// Left side
-		for (int i = 1; i <= row; i++)
+	for (int row = 1; row <= totalRows; row++) {
+		for (int i = 1; i <= row; i++) {
 			cout << "+";
-
-		// Space between sides
-		cout << " ";
-
-		// Right side
-		for (int i = 1; i <= totalRows + 1 - row; i++)
-			cout << "+";
-
+		}
 		cout << endl;
 	}
+
+	cout << endl; // Space between patterns
+
 
 	// Pattern B
 	cout << "Pattern B" << endl;
