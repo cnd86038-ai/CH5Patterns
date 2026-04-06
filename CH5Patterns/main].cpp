b3ce983df2a964b5c938eda5b44802119e483a71
cnd86038-ai <cnd86038@email.vccs.edu>
2026-04-06 17:13:50 -0400

    Making pattern A more consise.

--- CH5Patterns/main].cpp
+++ CH5Patterns/main].cpp
@@ -5,24 +5,22 @@ int main()
 {
 	const int totalRows = 10;
 
-	//Print Patter A
+	// Pattern A
 	cout << "Pattern A" << endl;
 	for (int row = 1; row <= totalRows; row++)
 	{
-		//Left side
+		// Left side
 		for (int i = 1; i <= row; i++)
-		{
 			cout << "+";
-		}
 
-		//Space 
+		// Space between sides
 		cout << " ";
 
 		// Right side
 		for (int i = 1; i <= totalRows + 1 - row; i++)
 			cout << "+";
 
-		cout << endl; // Next row
+		cout << endl;
 	}
 
 	// Pattern B
